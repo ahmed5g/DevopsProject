d5464543545c3c7de69a02ee1e63961d391460b0
Ahmed <ahmed.ghorbel1@esprit.tn>
2023-12-04 11:12:37 +0100

    Controller requests comment descriptions added

--- Backend/eventsProject/src/main/java/tn/esprit/eventsproject/controllers/EventRestController.java
+++ Backend/eventsProject/src/main/java/tn/esprit/eventsproject/controllers/EventRestController.java
@@ -16,22 +16,31 @@ import java.util.List;
 public class EventRestController {
     private final IEventServices eventServices;
 
+    //http://localhost:8089/events/event/addPart
     @PostMapping("/addPart")
     public Participant addParticipant(@RequestBody Participant participant){
         return eventServices.addParticipant(participant);
     }
+
+    //http://localhost:8089/events/event/addEvent/1
     @PostMapping("/addEvent/{id}")
     public Event addEventPart(@RequestBody Event event, @PathVariable("id") int idPart){
         return eventServices.addAffectEvenParticipant(event, idPart);
     }
+
+    //http://localhost:8089/events/event/addEvent
     @PostMapping("/addEvent")
     public Event addEvent(@RequestBody Event event){
         return eventServices.addAffectEvenParticipant(event);
     }
+
+    //http://localhost:8089/events/event/addAffectLog/anyDescription
     @PutMapping("/addAffectLog/{description}")
     public Logistics addAffectLog(@RequestBody Logistics logistics,@PathVariable("description") String descriptionEvent){
         return eventServices.addAffectLog(logistics,descriptionEvent);
     }
+
+    //http://localhost:8089/events/event/getLogs/yyyy-mm-dd/yyyy-mm-dd
     @GetMapping("/getLogs/{d1}/{d2}")
     public List<Logistics> getLogistiquesDates (@PathVariable("d1") LocalDate date_debut, @PathVariable("d2") LocalDate date_fin){
         return eventServices.getLogisticsDates(date_debut,date_fin);
